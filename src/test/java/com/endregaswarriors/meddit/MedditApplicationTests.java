8ed27c00adf46e04f2dfbba87ff3275060fc5060
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-04-21 16:24:07 +0200

    Initial commit

---
+++ src/test/java/com/endregaswarriors/meddit/MedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endregaswarriors.meddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
