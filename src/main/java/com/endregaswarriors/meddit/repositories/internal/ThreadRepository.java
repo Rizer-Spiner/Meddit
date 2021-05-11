56368e75589939e6cc1b1e3b970927786444e74a
JustelisJ <j.jancys@gmail.com>
2021-05-11 14:28:55 +0300

    Thread repository

---
+++ src/main/java/com/endregaswarriors/meddit/repositories/internal/ThreadRepository.java
@@ -0,0 +1,7 @@
+package com.endregaswarriors.meddit.repositories.internal;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ThreadRepository extends CrudRepository<Thread, Integer> {
+
+}
