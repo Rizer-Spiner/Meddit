2c9ad11dd704adc28b2564ba488c1282ca5f2386
JustelisJ <j.jancys@gmail.com>
2021-05-14 14:22:57 +0300

    Added Lombok to Thread class ThreadRepository import fix

--- src/main/java/com/endregaswarriors/meddit/repositories/internal/ThreadRepository.java
+++ src/main/java/com/endregaswarriors/meddit/repositories/internal/ThreadRepository.java
@@ -1,5 +1,6 @@
 package com.endregaswarriors.meddit.repositories.internal;
 
+import com.endregaswarriors.meddit.models.database.Thread;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ThreadRepository extends CrudRepository<Thread, Integer> {
