b4d7d26c62328feadaf60f13b01394ce90aee783
JustelisJ <j.jancys@gmail.com>
2021-05-17 15:15:49 +0300

    Updated Thread fields according to domain model

--- src/main/java/com/endregaswarriors/meddit/models/database/Thread.java
+++ src/main/java/com/endregaswarriors/meddit/models/database/Thread.java
@@ -14,13 +14,12 @@ import java.time.LocalDate;
 public class Thread {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer thread_id;
+    private Long thread_id;
 
     //TODO: add foreign key constraint
     //private Integer movie_id;
     private Integer user_id;
     private String title;
     private String content;
-    private Integer counter;
     private LocalDate postDate;
 }
