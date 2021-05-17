4cfc7d0b011e1fae91cf9c1c4d5fb2f7cae86408
JustelisJ <j.jancys@gmail.com>
2021-05-17 15:15:49 +0300

    Updated Thread fields according to domain model

--- src/main/java/com/endregaswarriors/meddit/repositories/internal/ThreadRepository.java
+++ src/main/java/com/endregaswarriors/meddit/repositories/internal/ThreadRepository.java
@@ -3,6 +3,6 @@ package com.endregaswarriors.meddit.repositories.internal;
 import com.endregaswarriors.meddit.models.database.Thread;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ThreadRepository extends CrudRepository<Thread, Integer> {
+public interface ThreadRepository extends CrudRepository<Thread, Long> {
 
 }
