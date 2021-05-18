f160d3e15ea193c48dfb81c60c42cbc89a2abaa5
JustelisJ <j.jancys@gmail.com>
2021-05-18 15:53:56 +0300

    Subreddit repository created

---
+++ src/main/java/com/endregaswarriors/meddit/repositories/internal/SubredditRepository.java
@@ -0,0 +1,8 @@
+package com.endregaswarriors.meddit.repositories.internal;
+
+import com.endregaswarriors.meddit.models.database.Subreddit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubredditRepository extends CrudRepository<Subreddit, Integer> {
+
+}
