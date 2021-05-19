48d608f7035a22931ee9044984ac82fda1b4e539
JustelisJ <j.jancys@gmail.com>
2021-05-19 17:58:14 +0300

    MovieRedditService interface creation

--- src/main/java/com/endregaswarriors/meddit/services/MovieRedditService.java
+++ src/main/java/com/endregaswarriors/meddit/services/MovieRedditService.java
@@ -1,4 +1,16 @@
 package com.endregaswarriors.meddit.services;
 
-public class MovieRedditService {
+import com.endregaswarriors.meddit.models.Movie;
+import com.endregaswarriors.meddit.models.MovieSearchResult;
+import com.endregaswarriors.meddit.models.Response;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public interface MovieRedditService {
+
+    CompletableFuture<List<MovieSearchResult>> searchForMovie(String keyword);
+
+    CompletableFuture<Response<Movie>> getMovieDetails(Integer IMDB_id);
+
 }
