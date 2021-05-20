aba20f7531b4bbdd8b1b7d1fc9be8e91ce24a20c
JustelisJ <j.jancys@gmail.com>
2021-05-20 14:52:39 +0300

    Fixed SubredditMember mapping to database SubredditMemberPK implements serializable

--- src/main/java/com/endregaswarriors/meddit/models/database/SubredditMember.java
+++ src/main/java/com/endregaswarriors/meddit/models/database/SubredditMember.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "subredditmembers")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/endregaswarriors/meddit/models/database/keys/SubredditMemberPK.java
+++ src/main/java/com/endregaswarriors/meddit/models/database/keys/SubredditMemberPK.java
@@ -3,10 +3,11 @@ package com.endregaswarriors.meddit.models.database.keys;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Embeddable
 @Data
-public class SubredditMemberPK {
+public class SubredditMemberPK implements Serializable {
 
     protected Integer subreddit_id;
     protected Integer user_id;
