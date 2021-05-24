84b0e04e904cc885e95e78df2666aa41ab4c8916
JustelisJ <j.jancys@gmail.com>
2021-05-24 12:17:55 +0300

    Thread saving to database test Entity id strategy changed to IDENTITY

--- src/main/java/com/endregaswarriors/meddit/models/database/Subreddit.java
+++ src/main/java/com/endregaswarriors/meddit/models/database/Subreddit.java
@@ -1,6 +1,9 @@
 package com.endregaswarriors.meddit.models.database;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,11 +13,18 @@ import java.time.LocalDate;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subreddit {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer subreddit_id;
 
     private Integer movie_id;
     private LocalDate created;
+
+    public Subreddit(Integer movie_id){
+        this.movie_id = movie_id;
+    }
 }
