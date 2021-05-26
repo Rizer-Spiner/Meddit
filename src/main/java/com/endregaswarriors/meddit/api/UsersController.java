7bea13848630794a5d409ab0bd1dcfb83dc941c1
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-05-26 10:15:18 +0200

    UsersController implementation

--- src/main/java/com/endregaswarriors/meddit/api/UsersController.java
+++ src/main/java/com/endregaswarriors/meddit/api/UsersController.java
@@ -1,4 +1,67 @@
 package com.endregaswarriors.meddit.api;
 
+import com.endregaswarriors.meddit.models.MovieSearchResult;
+import com.endregaswarriors.meddit.models.api.JoinSubreddit;
+import com.endregaswarriors.meddit.models.api.LoginUser;
+import com.endregaswarriors.meddit.models.api.NewUser;
+import com.endregaswarriors.meddit.models.database.MedditUser;
+import com.endregaswarriors.meddit.services.MedditUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/meddit/user")
+@Api(tags = {"user"})
 public class UsersController extends ControllerBase{
+
+    MedditUserService medditUserService;
+
+    public UsersController(MedditUserService medditUserService)
+    {
+        this.medditUserService = medditUserService;
+    }
+
+    @ApiOperation(value = "Sign in a new user to the Meddit platform", response = MovieSearchResult[].class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = " User successfully created/sign in"),
+            @ApiResponse(code = 500, message = "Internal error")
+    })
+    @PostMapping("")
+    CompletableFuture<ResponseEntity<MedditUser>> signUserIn(@RequestBody NewUser newUser)
+    {
+        return medditUserService.signUserIn(newUser).thenCompose(this::map);
+    }
+
+    @ApiOperation(value = "Log in a user to the Meddit platform", response = MovieSearchResult[].class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = " User successfully logged in"),
+            @ApiResponse(code = 401, message = " User is unauthorized due to failed login "),
+            @ApiResponse(code = 500, message = "Internal error")
+    })
+    @PostMapping("")
+    CompletableFuture<ResponseEntity<MedditUser>> loginUser(@RequestBody LoginUser loginUser)
+    {
+        return medditUserService.loginUser(loginUser).thenCompose(this::map);
+    }
+
+    @ApiOperation(value = "Join or leave a subreddit platform", response = MovieSearchResult[].class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = " Successfully updated membership"),
+            @ApiResponse(code = 500, message = "Internal error")
+    })
+    @PatchMapping("")
+    CompletableFuture<ResponseEntity<Void>> joinOrLeaveSubreddit(@RequestBody JoinSubreddit joinSubreddit)
+    {
+        if (joinSubreddit.isJoin())
+        return medditUserService.joinSubreddit(joinSubreddit.getThreadId(), joinSubreddit.getUserId()).thenCompose(this::map);
+        else return medditUserService.leaveSubreddit(joinSubreddit.getThreadId(), joinSubreddit.getUserId()).thenCompose(this::map);
+    }
+
+
 }
--- src/main/java/com/endregaswarriors/meddit/services/MedditUserService.java
+++ src/main/java/com/endregaswarriors/meddit/services/MedditUserService.java
@@ -4,16 +4,17 @@ import com.endregaswarriors.meddit.models.Response;
 import com.endregaswarriors.meddit.models.api.LoginUser;
 import com.endregaswarriors.meddit.models.api.NewUser;
 import com.endregaswarriors.meddit.models.database.MedditUser;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 
+@Service
 public interface MedditUserService {
 
     CompletableFuture<Response<MedditUser>> signUserIn(NewUser newUser);
     CompletableFuture<Response<MedditUser>> loginUser(LoginUser loginUser);
-    //joinSubreddit
-    //leaveSubreddit
-    //upvoteThread
-    //upvoteComment
-    //addFavoriteMovieSubreddit
+    CompletableFuture<Response<Void>> joinSubreddit(Long thread_id, Integer user_id);
+    CompletableFuture<Response<Void>> leaveSubreddit(Long thread_id, Integer user_id);
+    CompletableFuture<Response<Void>> upvoteThread(Long thread_id, Integer user_id, Boolean upvote);
+    CompletableFuture<Response<Void>> upvoteComment(Long comment_id, Integer user_id, Boolean upvote);
 }
