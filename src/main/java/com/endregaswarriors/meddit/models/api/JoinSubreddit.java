91fbfde3d45229fd195cf565345b8f2e09867b14
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-05-26 10:15:18 +0200

    UsersController implementation

---
+++ src/main/java/com/endregaswarriors/meddit/models/api/JoinSubreddit.java
@@ -0,0 +1,17 @@
+package com.endregaswarriors.meddit.models.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinSubreddit {
+
+    private Long threadId;
+    private Integer userId;
+    private boolean join;
+}
