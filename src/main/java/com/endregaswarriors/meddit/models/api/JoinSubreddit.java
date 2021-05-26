b60650508cc975a23d5bcc55483dc95fa1baafd0
Rizer-Spiner <spiridon.roxana1999@gmail.com>
2021-05-26 13:44:59 +0200

    Fixup MedditUserService interface

--- src/main/java/com/endregaswarriors/meddit/models/api/JoinSubreddit.java
+++ src/main/java/com/endregaswarriors/meddit/models/api/JoinSubreddit.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class JoinSubreddit {
 
-    private Long threadId;
+    private Integer threadId;
     private Integer userId;
     private boolean join;
 }
