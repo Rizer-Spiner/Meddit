ded11ee190f07ee74f5203d1cb1760454d9ff7cd
JustelisJ <j.jancys@gmail.com>
2021-05-28 15:26:08 +0300

    Trafficker service created

---
+++ src/main/java/com/endregaswarriors/meddit/services/TraffickerService.java
@@ -0,0 +1,7 @@
+package com.endregaswarriors.meddit.services;
+
+public interface TraffickerService {
+
+    void logTraffic(Integer subreddit_id, Integer user_id);
+
+}
---
+++ src/main/java/com/endregaswarriors/meddit/services/TraffickerServiceImpl.java
@@ -0,0 +1,29 @@
+package com.endregaswarriors.meddit.services;
+
+import com.endregaswarriors.meddit.models.database.MedditUser;
+import com.endregaswarriors.meddit.models.database.Trafficker;
+import com.endregaswarriors.meddit.models.database.keys.TraffickerPK;
+import com.endregaswarriors.meddit.repositories.internal.TraffickerRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class TraffickerServiceImpl implements TraffickerService {
+
+    TraffickerRepository traffickerRepository;
+
+    public TraffickerServiceImpl(TraffickerRepository traffickerRepository) {
+        this.traffickerRepository = traffickerRepository;
+    }
+
+    @Override
+    public void logTraffic(Integer subreddit_id, Integer user_id) {
+        Trafficker trafficker = Trafficker.builder()
+                .traffickerPK(TraffickerPK.builder().subreddit_id(subreddit_id).timestamp(LocalDateTime.now()).build())
+                .user_id(MedditUser.builder().user_id(user_id).build())
+                .build();
+
+        traffickerRepository.save(trafficker);
+    }
+}
