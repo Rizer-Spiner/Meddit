8f63293f40d590d510348fc17397bd549017e0d0
JustelisJ <j.jancys@gmail.com>
2021-05-30 13:32:31 +0300

    Updated model classes from statistics

--- src/main/java/com/endregaswarriors/meddit/models/MovieFavoriteReport.java
+++ src/main/java/com/endregaswarriors/meddit/models/MovieFavoriteReport.java
@@ -1,4 +1,15 @@
 package com.endregaswarriors.meddit.models;
 
+import com.endregaswarriors.meddit.models.database.statistics.FavoriteMovieStats;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class MovieFavoriteReport {
+
+    List<FavoriteMovieStats> favoriteMovieStats;
+
 }
--- src/main/java/com/endregaswarriors/meddit/models/api/GetMovieReport.java
+++ src/main/java/com/endregaswarriors/meddit/models/api/GetMovieReport.java
@@ -2,13 +2,9 @@ package com.endregaswarriors.meddit.models.api;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 public class GetMovieReport {
 
     private Integer subreddit_id;
-    private LocalDate from;
-    private LocalDate to;
 
 }
